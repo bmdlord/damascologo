219f19fabdc32769f6fe74f931f5518337c40afb
bmdlord <66489141+bmdlord@users.noreply.github.com>
2020-09-06 00:30:23 +0800

    Add files via upload

---
+++ damascologo.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+	SHOPIFY
+</title>
+</head>	
+<link rel="stylesheet" href="css.css" type="text/css">
+<body> 
+<div class="container">
+<header>
+<div class="picture1">
+<div class="pics">
+<img src="damascologo.png" alt=""> 
+</div>
+<br>
+<br>
+<li><a class="btn16"> HOME </a></li>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<li><a class="btn17"> ABOUT </a></li>
+
+</header>
+  <main class="main">
+    <h1>ONLINE FOODSHOP</h1>
+	<div class="button">
+		<a href="" class="btn btn-one"> ORDER NOW<a/>
+		
+		</div>
+		<br>
+		<h2>COME AND TRY OUR DELICIOUS DELICACIES</h2>
+	
+		<br>
+		<div class="picture">
+		<center><img src="food1.jfif"></center>
+		</div>
+		
+  </main>
+    <footer class="footer">
+	<p>ENJOY YOUR MEAL</p>
+	
+
+  </footer>
+
+</div>
+
+
+</body>
+</html>
+	
